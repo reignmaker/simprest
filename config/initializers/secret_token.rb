53493f6d53e3fb7e27866cb936f1ed6ee2ff951b
reignmaker <reignmaker@xakep.ru>
2013-12-16 22:16:34 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Simprest::Application.config.secret_key_base = 'aaa521199893cf6db24267c75d9e40f455260c69b341fecb641ce28973efad9dd9d8d89fd91e6b5bebd2a65d31a06724eaff7ed8f25e5b7cfaded2819d5165bc'
