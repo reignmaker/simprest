4b0951adb98b625cb6b17e25dfffc9d59d0a4235
reignmaker <reignmaker@xakep.ru>
2013-12-17 14:26:10 +0400

    user responders, dump action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < InheritedResources::Base
+  respond_to :json
+  respond_to :html, :except => [:dump]
+
+  def dump
+    @users = User.includes(:tickets)
+  end
 
   def build_resource_params
     [params.fetch(:user, {}).permit(:email, :referrer)]
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Simprest::Application.routes.draw do
 
   root :to => 'users#index'
 
+
   resources :users do
     resources :tickets
+    constraints :format => :json do
+      get 'dump' => 'users#dump', :on => :collection, :defaults => {:format => :json}
+    end
   end
   
 end
