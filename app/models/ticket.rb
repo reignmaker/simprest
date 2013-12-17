b743d01ce7635f3a078b4130d701e2b20ac31b9f
reignmaker <reignmaker@xakep.ru>
2013-12-17 12:50:22 +0400

    ticket before_create callback

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,11 @@
 class Ticket < ActiveRecord::Base
   belongs_to :user
   validates :user, :presence => true
+  before_create :sign_ticket
+
+  private
+  def sign_ticket
+    self.key = SecureRandom.hex
+  end
+  
 end
