54012fe425d2288a95263b580fc77fff123c93c5
reignmaker <reignmaker@xakep.ru>
2013-12-19 17:02:51 +0400

    null_session for json requests

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,4 +1,7 @@
 class TicketsController < InheritedResources::Base
+  protect_from_forgery with: :null_session, :if => 
+    Proc.new { |c| c.request.format == 'application/json' }
+    
   respond_to :html, :json
   belongs_to :user
   def build_resource_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < InheritedResources::Base
+  protect_from_forgery with: :null_session, :if => 
+    Proc.new { |c| c.request.format == 'application/json' }
+
   respond_to :json
   respond_to :html, :except => [:dump]
 
